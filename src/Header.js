37c6e3c50557ccdf0291ef9701a5edb315428dbe
ahsenyavas <57hasanyavas@gmail.com>
2022-01-31 16:31:12 +0300

    development

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div className="header">
         <h1>Recipe Search</h1>
-        <p style={{color:"rgb(239, 187, 243)"}}> This Edamam recipe API has the data of tens of thousands of foods, including international dishes.</p>
+        <p style={{color:"rgb(239, 187, 243)"}}> *This Edamam recipe API has the data of tens of thousands of foods, including international dishes.</p>
     </div>
   );
 };
